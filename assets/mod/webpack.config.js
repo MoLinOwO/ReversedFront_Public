48f16c592d8d035e6c6b5c093e16ddb84ed50701
molinowo <moyulive1825@gmail.com>
2026-01-06 02:27:28 +0800

    Feat: Add code protection for production builds
    - Enable Terser minification and obfuscation
    - Only bundle compiled JS in release, exclude source code
    - Source code remains on GitHub for development

--- assets/mod/webpack.config.js
+++ assets/mod/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './js/index.js',
@@ -9,6 +10,26 @@ module.exports = {
     publicPath: './mod/js/'
   },
   mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+            drop_debugger: true,
+          },
+          mangle: {
+            toplevel: true,
+          },
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  },
   module: {
     rules: [
       // 如需 babel-loader、css-loader 可於此擴充
